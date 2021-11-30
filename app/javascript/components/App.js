daf1245ca26108b5e3ed99972e880bdc2879aee6
Rhyine <rhyinestewart@gmail.com>
2021-11-30 00:37:18 -0500

    Style app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
       // </React.Fragment>
     <React.StrictMode>
       <Provider store={store}>
-      <div>
+      <div className="cont">
         <Greeting />
       </div>
       </Provider>
